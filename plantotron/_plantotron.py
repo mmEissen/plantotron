4b5a9f1b4d2f9d66c63db6738f908cc41670e11a
Momo Eissenhauer <momo.eissenhauer@gmail.com>
2023-06-01 23:48:26 +0200

    this is probably dumb

--- plantotron/_plantotron.py
+++ plantotron/_plantotron.py
@@ -3,21 +3,42 @@ import os
 import time
 from plantotron import _spreadsheet
 import simple_rpc
+import simple_pid
 
 
 DEVICE = "/dev/ttyACM0"
 INTERFACE_FILE = os.path.join(os.path.dirname(__file__), "interface.yml")
-SENSOR_IDS = (0, 1, 2, 3)
+
 
 class Application:
-    def __init__(self) -> None:
+    def __init__(self, config: _spreadsheet.Config) -> None:
         self.loop_delay = 60 * 15
         self.interface = simple_rpc.Interface(DEVICE, autoconnect=False)
         with open(INTERFACE_FILE) as interface_file:
             self.interface.open(interface_file)
+        self.config = _spreadsheet.load_config()
+        self.pid_controllers = {
+            plant_id: simple_pid.PID() for plant_id in self.config.pid_configs
+        }
+        self._configure_pid_controllers()
+
+    def _configure_pid_controllers(self) -> None:
+        for plant_id, pid_config in self.pid_configs:
+            try:
+                pid_controller = self.config.pid_configs[plant_id]
+            except KeyError:
+                pid_controller = self.pid_controllers[plant_id] = simple_pid.PID()
+            pid_controller.Kp = pid_config.proportional_gain
+            pid_controller.Ki = pid_config.integral_gain
+            pid_controller.Kd = pid_config.derivative_gain
+
+    def plant_ids(self) -> tuple[int, int, int, int]:
+        return tuple(sorted(self.config.pid_configs.keys()))
 
     def _read_all_sensors(self) -> _spreadsheet.SensorData:
-        return tuple(self.interface.get_sensor(sensor_id) for sensor_id in SENSOR_IDS)
+        return tuple(
+            self.interface.get_sensor(sensor_id) for sensor_id in self.plant_ids()
+        )
 
     def _average_sensor_readings(
         self, n: int = 10, delta_t: float = 0.1
@@ -29,9 +50,16 @@ class Application:
         return tuple(sum(samples) / n for samples in zip(*data))
 
     def loop(self) -> None:
+        self.config = _spreadsheet.load_config()
+        self._configure_pid_controllers()
         now = datetime.datetime.now()
         data = self._average_sensor_readings()
-        _spreadsheet.append_sensor_data(now, data)
+        controls = tuple(
+            self.pid_controllers[plant_id](value)
+            for plant_id, value in zip(self.plant_ids(), data)
+        )
+        _spreadsheet.append_sensor_data(now, tuple(*data, *controls))
+        
 
     def run_forever(self) -> None:
         while True:
--- plantotron/_spreadsheet.py
+++ plantotron/_spreadsheet.py
@@ -1,3 +1,4 @@
+import dataclasses
 import datetime
 import functools
 import os
@@ -11,8 +12,10 @@ SERVICE_ACCOUNT_FILE = os.path.join(
 )
 SPREADSHEET_ID = "1zdE2TLWVGPYxgmjvDDOAKpG-9qtT27OLOew0wa4K8qI"
 SENSOR_DATA_SHEET = "sensor_data"
-SENSOR_DATA_RANGE = "A:E"
+SENSOR_DATA_RANGE = "A:I"
 
+CONFIG_SHEET = "config"
+CONFIG_RANGE = "A2:E5"
 
 SensorData = tuple[float, float, float, float]
 
@@ -28,7 +31,7 @@ def _service():
     return googleapiclient.discovery.build("sheets", "v4", credentials=_credentials())
 
 
-def append_sensor_data(time: datetime.datetime, data: tuple[float, float, float, float]):
+def append_sensor_data(time: datetime.datetime, data: tuple[float, ...]):
     request = _service().spreadsheets().values().append(
         spreadsheetId=SPREADSHEET_ID,
         range=f"{SENSOR_DATA_SHEET}!{SENSOR_DATA_RANGE}",
@@ -43,3 +46,36 @@ def append_sensor_data(time: datetime.datetime, data: tuple[float, float, float,
     )
     return request.execute()
 
+
+@dataclasses.dataclass
+class PIDConfig:
+    setpoint: float
+    proportional_gain: float
+    integral_gain: float
+    derivative_gain: float
+
+
+@dataclasses.dataclass
+class Config:
+    pid_configs: dict[int, PIDConfig]
+
+
+def load_config() -> Config:
+    request = _service().spreadsheets().values().get(
+        spreadsheetId=SPREADSHEET_ID,
+        range=f"{CONFIG_SHEET}!{CONFIG_RANGE}",
+        majorDimension="ROWS",
+        valueRenderOption="UNFORMATTED_VALUE",
+    )
+    response = request.execute()
+    return Config(
+        pid_configs={
+            plant_id: PIDConfig(
+                setpoint=setpoint,
+                proportional_gain=p,
+                integral_gain=i,
+                derivative_gain=d,
+            )
+            for plant_id, setpoint, p, i, d in response["values"]
+        }
+    )
