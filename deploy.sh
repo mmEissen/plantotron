7ed21164ca8e8ece781eaf4c3ce68b7b3bbb7539
Momo Eissenhauer <momo.eissenhauer@gmail.com>
2023-06-01 21:19:36 +0200

    print dates

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,14 @@ set -ex
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
+date
 git fetch
+date
 git checkout $GITHUB_SHA
-
+date
 /home/momo/.local/bin/poetry install -n --ansi --without=dev --sync
-
+date
 POETRY_PYTHON="$(/home/momo/.local/bin/poetry run -n --ansi which python)"
+date
 /usr/bin/sudo "${POETRY_PYTHON}" -m plantotron install
-
+date
